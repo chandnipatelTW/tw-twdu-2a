f9c7e02a0916cd55c80482f12ee58c2515430f62
yudong <yudong@thoughtworks.com>
2019-11-26 11:15:00 +0530

    [#1][DongYu] Fail the pipeline when tests fail

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean test bootJar
+
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt test && sbt package
-cd $DIR/../StationConsumer && sbt test && sbt package
-cd $DIR/../StationTransformerNYC && sbt test && sbt package
+cd $DIR/../RawDataSaver
+sbt test
+sbt package
+
+cd $DIR/../StationConsumer
+sbt test
+sbt package
+
+cd $DIR/../StationTransformerNYC
+sbt test
+sbt package
