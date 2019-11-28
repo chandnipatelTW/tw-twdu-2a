fcfba24c1e58c597576536dcd049179cd8b420f9
ext_mshaikh <mshaikh@thoughtworks.com>
2019-11-28 12:17:09 +0530

    [Ankita,Mujir]- stationMart csv streaming output can now be read using spark.readStream

--- StationConsumer/src/main/scala/com/free2wheelers/apps/StationApp.scala
+++ StationConsumer/src/main/scala/com/free2wheelers/apps/StationApp.scala
@@ -70,8 +70,9 @@ object StationApp {
       .reduceGroups((r1,r2)=>if (r1.last_updated > r2.last_updated) r1 else r2)
       .map(_._2)
       .writeStream
+      .partitionBy("last_updated")
+      .outputMode("update")
       .format("overwriteCSV")
-      .outputMode("complete")
       .option("header", true)
       .option("truncate", false)
       .option("checkpointLocation", checkpointLocation)
