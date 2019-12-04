2809e905f99d268406ef8dcbaf9a0a44a7658db5
lastorder <yudong@thoughtworks.com>
2019-12-04 16:26:30 +0530

    [#8][Pooja & Dongyu] change to use new kafka master

--- sbin/deploy-qa.sh
+++ sbin/deploy-qa.sh
@@ -33,12 +33,12 @@ Host bastion.twdu-2a-qa.training
 echo "====SSH Config Updated===="
 
 echo "====Insert app config in zookeeper===="
-scp ./zookeeper/seed.sh kafka.twdu-2a-qa.training:/tmp/zookeeper-seed.sh
-ssh kafka.twdu-2a-qa.training '
+scp ./zookeeper/seed.sh kafka-1.twdu-2a-qa.training:/tmp/zookeeper-seed.sh
+ssh kafka-1.twdu-2a-qa.training '
 set -e
 export hdfs_server="emr-master.twdu-2a-qa.training:8020"
-export kafka_server="kafka.twdu-2a-qa.training:9092"
-export zk_command="zookeeper-shell kafka.twdu-2a-qa.training:2181"
+export kafka_server="kafka-1.twdu-2a-qa.training:9092"
+export zk_command="zookeeper-shell kafka-1.twdu-2a-qa.training:2181"
 sh /tmp/zookeeper-seed.sh
 '
 echo "====Inserted app config in zookeeper===="
@@ -80,7 +80,7 @@ kill_process ${test_station_sf_qa}
 echo "====Runing Producers Killed===="
 
 echo "====Deploy Producers===="
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${test_station_sf_qa} --producer.topic=station_data_sf --kafka.brokers=kafka.twdu-2a-qa.training:9092 1>/tmp/${test_station_sf_qa}.log 2>/tmp/${test_station_sf_qa}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${test_station_sf_qa} --producer.topic=station_data_sf --kafka.brokers=kafka-1.twdu-2a-qa.training:9092 1>/tmp/${test_station_sf_qa}.log 2>/tmp/${test_station_sf_qa}.error.log &
 echo "====Producers Deployed===="
 
 echo "====Start mock server===="
@@ -123,7 +123,7 @@ echo "====Old Raw Data Saver Killed===="
 
 echo "====Deploy Raw Data Saver===="
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka.twdu-2a-qa.training:2181 "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka-1.twdu-2a-qa.training:2181 "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
 echo "====Raw Data Saver Deployed===="
 '
@@ -150,7 +150,7 @@ echo "====Old Station Consumers Killed===="
 
 echo "====Deploy Station Consumers===="
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/free2wheelers-station-consumer_2.11-0.0.1.jar kafka.twdu-2a-qa.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/free2wheelers-station-consumer_2.11-0.0.1.jar kafka-1.twdu-2a-qa.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
 
 echo "====Station Consumers Deployed===="
 '
--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -33,12 +33,12 @@ Host bastion.twdu-2a.training
 echo "====SSH Config Updated===="
 
 echo "====Insert app config in zookeeper===="
-scp ./zookeeper/seed.sh kafka.twdu-2a.training:/tmp/zookeeper-seed.sh
-ssh kafka.twdu-2a.training '
+scp ./zookeeper/seed.sh kafka-1.twdu-2a.training:/tmp/zookeeper-seed.sh
+ssh kafka-1.twdu-2a.training '
 set -e
 export hdfs_server="emr-master.twdu-2a.training:8020"
-export kafka_server="kafka.twdu-2a.training:9092"
-export zk_command="zookeeper-shell kafka.twdu-2a.training:2181"
+export kafka_server="kafka-1.twdu-2a.training:9092"
+export zk_command="zookeeper-shell kafka-1.twdu-2a.training:2181"
 sh /tmp/zookeeper-seed.sh
 '
 echo "====Inserted app config in zookeeper===="
@@ -84,10 +84,10 @@ echo "====Runing Producers Killed===="
 
 echo "====Deploy Producers===="
 
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_information} --kafka.brokers=kafka.twdu-2a.training:9092 1>/tmp/${station_information}.log 2>/tmp/${station_information}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka.twdu-2a.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_status} --kafka.brokers=kafka.twdu-2a.training:9092 1>/tmp/${station_status}.log 2>/tmp/${station_status}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_marseille} --kafka.brokers=kafka.twdu-2a.training:9092 1>/tmp/${station_marseille}.log 2>/tmp/${station_marseille}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_information} --kafka.brokers=kafka-1.twdu-2a.training:9092 1>/tmp/${station_information}.log 2>/tmp/${station_information}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka-1.twdu-2a.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_status} --kafka.brokers=kafka-1.twdu-2a.training:9092 1>/tmp/${station_status}.log 2>/tmp/${station_status}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_marseille} --kafka.brokers=kafka-1.twdu-2a.training:9092 1>/tmp/${station_marseille}.log 2>/tmp/${station_marseille}.error.log &
 
 echo "====Producers Deployed===="
 '
@@ -128,13 +128,13 @@ echo "====Old Raw Data Saver Killed===="
 
 echo "====Deploy Raw Data Saver===="
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka.twdu-2a.training:2181 "/free2wheelers/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 "/free2wheelers/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka.twdu-2a.training:2181 "/free2wheelers/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 "/free2wheelers/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka.twdu-2a.training:2181 "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka.twdu-2a.training:2181 "/free2wheelers/stationDataMarseille" 1>/tmp/raw-station-data-marseille-saver.log 2>/tmp/raw-station-data-marseille-saver.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 "/free2wheelers/stationDataMarseille" 1>/tmp/raw-station-data-marseille-saver.log 2>/tmp/raw-station-data-marseille-saver.error.log &
 
 echo "====Raw Data Saver Deployed===="
 '
@@ -163,9 +163,9 @@ echo "====Old Station Consumers Killed===="
 
 echo "====Deploy Station Consumers===="
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M /tmp/free2wheelers-station-consumer_2.11-0.0.1.jar kafka.twdu-2a.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M /tmp/free2wheelers-station-consumer_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
 
-nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M /tmp/free2wheelers-station-transformer-nyc_2.11-0.0.1.jar kafka.twdu-2a.training:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
+nohup spark-submit --master yarn --queue streaming --deploy-mode cluster --class com.free2wheelers.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --driver-memory 500M /tmp/free2wheelers-station-transformer-nyc_2.11-0.0.1.jar kafka-1.twdu-2a.training:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
 
 echo "====Station Consumers Deployed===="
 '
