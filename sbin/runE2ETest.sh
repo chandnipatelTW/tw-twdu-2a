2f52830579c25d81da9d96360724957d0ec5d297
AnkiBhatia <57541322+AnkiBhatia@users.noreply.github.com>
2019-12-05 17:56:35 +0530

    Revert "#28 Update SSH config for e2e test job"
    This reverts commit fe6cdf8b3b0b02e08d43775a80184e5d246c00f4.

--- sbin/runE2ETest.sh
+++ sbin/runE2ETest.sh
@@ -1,37 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
-
-
-echo "====Updating SSH Config===="
-
-
-echo "
-
-Host emr-master.twdu-2a-qa.training
-    User hadoop
-
-Host !github.com *
-	User ec2-user
-	IdentitiesOnly yes
-	ForwardAgent yes
-	DynamicForward 6789
-    StrictHostKeyChecking no
-
-
-Host *.twdu-2a-qa.training !bastion.twdu-2a-qa.training
-    User ec2-user
-    ForwardAgent yes
-    ProxyCommand ssh bastion.twdu-2a-qa.training -W %h:%p 2>/dev/null
-
-Host bastion.twdu-2a-qa.training
-    User ec2-user
-    HostName ec2-52-221-9-233.ap-southeast-1.compute.amazonaws.com
-    DynamicForward 6789
-
-" >> ~/.ssh/config
-
-nohup ssh -L 2181:kafka-1.twdu-2a-qa.training:2181 bastion.twdu-2a-qa.training &
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Running E2E Tests===="
