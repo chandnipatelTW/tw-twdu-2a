54bf173a36673295bd96da5dacd044927f180c1d
AnkiBhatia <57541322+AnkiBhatia@users.noreply.github.com>
2019-12-05 12:24:43 +0530

    #28 | Ankita/Kaleeaswari | Removing mock server config, to run E2E tests in CI

--- sbin/runE2ETest.sh
+++ sbin/runE2ETest.sh
@@ -2,18 +2,20 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
-echo "====Building Consumer JARs===="
-cd $DIR/../StationConsumer && sbt package
+#echo "====Building Producer JARs===="
+#$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+#echo "====Building Consumer JARs===="
+#cd $DIR/../StationConsumer && sbt package
 echo "====Running docker-compose===="
 
-docker-compose -f $DIR/../docker/docker-compose-test.yml up --build -d
+docker-compose -f $DIR/../docker/docker-compose-e2e-test.yml up --build -d
 
-docker-compose -f $DIR/../docker/docker-compose-test.yml exec e2e-test sbt test
+docker-compose -f $DIR/../docker/docker-compose-e2e-test.yml exec e2e-test sbt test
 
 RESULT=$?
 
-docker-compose -f $DIR/../docker/docker-compose-test.yml down
+docker-compose -f $DIR/../docker/docker-compose-e2e-test.yml down
 
 exit "$RESULT"
+
+
